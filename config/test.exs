c54a9d5f51d7df8d595a66c50c494b6e8827465e
kedith <kedithke98@gmail.com>
2020-05-12 18:27:11 +0300

    added postgres in docker compose

--- config/test.exs
+++ config/test.exs
@@ -2,21 +2,24 @@ use Mix.Config
 
 config :conference, Conference.Repository.ArticleRepo,
        username: "postgres",
-       password: "admin",
+       password: "postgres",
        database: "conference_test",
        hostname: "localhost",
+       port: "5432",
        pool: Ecto.Adapters.SQL.Sandbox
 
 config :conference, Conference.Repository.UserDetailsRepo,
        username: "postgres",
-       password: "admin",
+       password: "postgres",
        database: "conference_test",
        hostname: "localhost",
+       port: "5432",
        pool: Ecto.Adapters.SQL.Sandbox
 
 config :conference, Conference.Repository.TalkRepo,
        username: "postgres",
-       password: "admin",
+       password: "postgres",
        database: "conference_test",
        hostname: "localhost",
+       port: "5432",
        pool: Ecto.Adapters.SQL.Sandbox
