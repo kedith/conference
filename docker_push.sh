ad3b72b54665188ba01a76b4cd4300ba9dc19fd9
kedith <kedithke98@gmail.com>
2020-05-29 22:37:46 +0300

    updated docker_push file

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push kedith/conference
+docker build -t conference .
+docker push kedith/conference:1.0.0
