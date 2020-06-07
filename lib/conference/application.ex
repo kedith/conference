77fe0fbee4413381dcdecd0f9fbe824d7e2701ab
kedith <kedithke98@gmail.com>
2020-06-07 19:59:56 +0300

    change port to 4000

--- lib/conference/application.ex
+++ lib/conference/application.ex
@@ -12,7 +12,7 @@ defmodule Conference.Application do
         scheme: :http,
         plug: Conference.Plug.Router,
         options: [
-          port: 8080
+          port: 4000
         ]
       },
       {Conference.Consumer, []},
